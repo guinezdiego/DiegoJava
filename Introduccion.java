0f2e163f6f1364ebe646926102ae674429271df9
guinezdiego <guinezdiego3103@gmail.com>
2026-07-14 01:57:19 -0400

    Curso Java

---
+++ Introduccion.java
@@ -0,0 +1,18 @@
+public class Introduccion {
+    public static void main(String[] args) {
+        System.out.println("Hola Java");
+        System.out.println("Esto sera divertido");
+        System.out.println("Este es mi primer programa de Java");
+
+        int cantidad = 10;
+        double decimal = 6.7;
+        System.out.println("cantidad: " + cantidad);
+        System.out.println("decimal: " + decimal);
+
+        String palabra = "Pan con mayo";
+        System.out.println(palabra);
+    }
+}
+
+// Si se puede
+
---
+++ clima.java
@@ -0,0 +1,18 @@
+public class clima {
+    public static void main(String[] args) {
+        int temperatura = 28;
+        int humedad = 65;
+        int velocidadViento = 15;
+
+        double temp_amanecer = 22.5;
+        double temp_tarde = 28.8;
+        double temp_noche = 23.4;
+
+        System.out.println("Temperatura actual: " + temperatura);
+        System.out.println("Nivel de humedad: " + humedad);
+        System.out.println("Velocidad del viento: " + velocidadViento);
+        System.out.println("Temperatura en la mañana: " + temp_amanecer);
+        System.out.println("Temperatura en la tarde: " + temp_tarde);
+        System.out.println("Temperatura en la noche: " + temp_noche);
+    }
+}
