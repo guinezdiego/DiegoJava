ee8a008edd090dd660eabca04f00bc62641f9254
guinezdiego <guinezdiego3103@gmail.com>
2026-08-25 21:21:11 -0400

    Curso Java

--- OperacionesString.java
+++ OperacionesString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class OperacionesString {
 
     public static void analizarTexto(String texto) {
@@ -40,11 +42,30 @@ public class OperacionesString {
         String mayus = primeros5.toUpperCase();
 
         System.out.println("First 5 chars: " + primeros5);
-        System.out.println("Uppercase: " + mayus);
+        System.out.println("Uppercase: " + mayus); 
+    }
+
+    public static String formatCourseInfo(String wuaja) {
+        String[] arr = wuaja.split(":");
+        return "Course Code: " + arr[0] +
+        "\nCourse Name: " + arr[1] +
+        "\nDays: " + arr[2] +
+        "\nTime: " + arr[3];
+    }
+
+    public static String createCourseList(String[] wipig) {
+        String text = String.join(", ", wipig);
+        return "Available Courses: " + text;
+    }
+
+    public static String formatSchedule(String LS) {
+        String[] sepa = LS.split(",");
+        String days = String.join(" | ", sepa);
+        return days;
     }
 
     public static void main(String[] args) {
-        String mensaje = "Hola, mundo!";
+        // String mensaje = "Hola, mundo!";
 
         // // Metodos de cadenas
         // // length() devuelve el numero de caracteres en la cadena
@@ -81,6 +102,63 @@ public class OperacionesString {
         // int index = mensaje.indexOf("mundo");
         // System.out.println(index);
 
-        analizarTexto(mensaje);
+        // .split() divide una cadena en un array de subcadenas basandose en un delimitador
+        // Dividir por espacios en blanco
+        // String text = "manzana platano cereza";
+        // String[] frutas = text.split(" ");
+        // System.out.println(Arrays.toString(frutas));
+
+        // Dividir con un delimitador especifico
+        // String data = "jhon,25,new york";
+        // String[] dataArr = data.split(",");
+        // System.out.println(Arrays.toString(dataArr));
+
+        // String.join() combina los elementos de una iterable en una sola cadena
+        // Union basica
+        // String[] palabras = {"Hola", "Mundo", "Java"};
+        // String texto = String.join(" ", palabras);
+        // System.out.println(texto);
+
+        // Unir con un separador
+        // String[] frutas = {"pera", "uva", "sandia"};
+        // String linea = String.join("_", frutas);
+        // System.out.println(linea);
+
+        //analizarTexto(mensaje);
+        
+        // En Java, String.format() es un metodo para crear cadenas con formato
+        // Sintaxis: String formattedString = String.format("Texto con variables", arg1, arg2, ...)
+        // %s inserta una cadena
+        // %d inserta un entero
+        // %f inserta un flotante
+        // %b inserta un valor booleano
+        // %c inserta un caracter
+        // %n inserta un caracter de nueva linea
+        // %.2f inserta un flotante con sus 2 decimales
+
+        // String name = "Alice";
+        // int age = 30;
+        // double price = 19.99;
+
+        // String formato = String.format("Name: %s, Age: %d, Price: %.2f", name, age, price);
+        // System.out.println(formato);
+
+        String str1 = "hola";
+        String str2 = "hola";
+        String str3 = "HOLA";
+        String str4 = "mundo";
+
+        // .equals() compara una cadena con otra. Devuelve true si son IGUALES y false si no lo son
+        boolean eq1 = str1.equals(str2);
+        System.out.println(eq1);
+        // .equalsIgnoreCase() lo mismo pero ignora las mayusculas
+        boolean eq2 = str1.equalsIgnoreCase(str3);
+        System.out.println(eq2);
+        // .compareTo() compara dos cadenas segun sus valores Unicode. Devuelve 0 si son iguales
+        int cmp1 = str1.compareTo(str2);
+        System.out.println(cmp1);
+        // .compareToIgnoreCase() Ignora las mayusculas
+        int cmp2 = str1.compareToIgnoreCase(str4);
+        System.out.println(cmp2);
     }
 }
